edda162844af22546f1310976cbaec5c02676005
Joonmo Yang <dev@remagpie.com>
2021-08-02 23:55:20 +0900

    Remove parseoptions for ts files

--- .eslintrc.js
+++ .eslintrc.js
@@ -271,9 +271,6 @@ module.exports = {
 			files: ["*.ts", "*.tsx"],
 			plugins: ["@typescript-eslint"],
 			parser: "@typescript-eslint/parser",
-			parserOptions: {
-				project: "./tsconfig.json",
-			},
 			rules: tsRules,
 		},
 	],
